014aaa83efd09375f8a48fc5963409ac3c6742dc
Brian Cooke <brian.cooke@live.co.uk>
2018-05-14 16:09:34 +0100

    Created Author and Book classes with properties, constructors and getter and setters.

--- src/main/java/models/Author.java
+++ src/main/java/models/Author.java
@@ -1,4 +1,40 @@
 package models;
 
 public class Author {
+
+    private String firstName;
+    private String lastName;
+    private int id;
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Author() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
--- src/main/java/models/Book.java
+++ src/main/java/models/Book.java
@@ -1,4 +1,40 @@
 package models;
 
 public class Book {
+
+    private String title;
+    private String genre;
+    private int id;
+
+    public Book(String title, String genre) {
+        this.title = title;
+        this.genre = genre;
+    }
+
+    public Book() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
