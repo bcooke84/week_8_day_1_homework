0595ce43da1369f4b828c29a8befda116d402515
Brian Cooke <brian.cooke@live.co.uk>
2018-05-14 16:58:05 +0100

    Created DBBook class and updated Runner.

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,4 +1,6 @@
+import db.DBAuthor;
 import db.DBBook;
+import models.Author;
 import models.Book;
 
 import java.util.List;
@@ -9,16 +11,26 @@ public class Runner {
         Book book1 = new Book("Lord of The Rings", "Fantasy");
         Book book2 = new Book("1984", "Dystopian");
 
+        Author author1 = new Author("JRR", "Tolkein");
+        Author author2 = new Author("George", "Orwell");
+
         DBBook.save(book1);
         DBBook.save(book2);
 
+        DBAuthor.save(author1);
+        DBAuthor.save(author2);
+
         List<Book> books = DBBook.getBooks();
+        List<Author> authors = DBAuthor.getAuthors();
 
         book2.setTitle("1985");
+        author2.setFirstName("Barry");
 
         DBBook.update(book2);
+        DBAuthor.update(author2);
 
         Book foundBook = DBBook.find(book2.getId());
+        Author foundAuthor = DBAuthor.find(author2.getId());
 
     }
 
